225032163dddc67f2d03c2a820ebe6bf7e0f1c26
njf26 <farnsworth.natalie@gmail.com>
2020-09-19 15:53:14 -0600

    Uploaded dicee flutter app

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+//creating an unassigned var makes it automatically dynamic, able to hold any data type
+//avoid var and dynamic types because it can cause crazyy things
+
+//use a state FULL widget if stuff is gonna change. otherwise use state LESS widget
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 3;
+  int rightDiceNumber = 6;
+
+  void randomize() {
+    //set State triggers rebuild and check what's changed
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                //this is an anonymous function
+                randomize();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                randomize();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
